854c6a3ff1cda0decdeca25fec4062675454a82a
paulevalentine <62242813+paulevalentine@users.noreply.github.com>
2025-12-28 15:20:01 +0000

    Retyping of variables and classess to mainly suit python style.

--- build/lib/concreteBeam.py
+++ build/lib/concreteBeam.py
@@ -1,35 +1,36 @@
 import structuralConcrete
 import math
 
-class RectBeam(structuralConcrete.Concrete):
+class Beam(structuralConcrete.Concrete):
     """ Reinforced concrete beam calculations """
-    def __init__(self, fck, b, h, cover, link_diam, bar_diam):
+    def __init__(self, fck, b, h, cover, linkDiam, barDiam):
         super().__init__(fck)
         self.b = b
         self.h = h
         self.cover = cover
-        self.link_diameter = link_diam
-        self.bar_diameter = bar_diam
-        self.effective_depth = self.h - self.cover - self.link_diameter - self.bar_diameter / 2
+        self.linkDiam = linkDiam
+        self.barDiam = barDiam
+        self.effectiveDepth = self.h - self.cover - self.linkDiam - self.barDiam / 2
+
         self.gc = 1.5 # partial factor on material strength
         self.fyk = 500 # characteristic strength for high yield steel
         self.acc = 0.85
 
-    def bending_steel_required(self, moment: float)->float:
+    def bendingSteelReq(self, moment: float)->float:
         """ This function does not establish the steel if compression steel is required """
 
         delta = 1
         kMax = 0.6 * delta - 0.18 * delta ** 2 - 0.21
-        m_max = kMax * (self.b * self.effective_depth ** 2 * self.fck) * 10 ** -6
+        m_max = kMax * (self.b * self.effectiveDepth ** 2 * self.fck) * 10 ** -6
         if moment > m_max:
             print(
                 f'Compression steel is require. The maximum moment the section can take without compression reinforcement is {m_max :.2f}kNm')
             return 0
 
-        k = moment * 10 ** 6 / (self.b * self.effective_depth ** 2 * self.fck)
+        k = moment * 10 ** 6 / (self.b * self.effectiveDepth ** 2 * self.fck)
 
         # calculate the level arm
-        z = min(self.effective_depth / 2 * (1 + (1 - 3.53 * k) ** 0.50), 0.95 * self.effective_depth)
+        z = min(self.effectiveDepth / 2 * (1 + (1 - 3.53 * k) ** 0.50), 0.95 * self.effectiveDepth)
 
         # calculate the requited bending steel
         ast = moment * 10 ** 6 / (0.87 * 500 * z)
@@ -37,61 +38,61 @@ class RectBeam(structuralConcrete.Concrete):
 
         return ast
 
-    def shear_cap_no_links(self, ast_prov : float)->float:
+    def shearCapNoLinks(self, astProv : float)->float:
         """ Check the shear capacity of a section if no shear reinforcement is provided """
-        p1 = min(ast_prov / (self.b * self.effective_depth), 0.02)
-        kv = min(1 + (200 / self.effective_depth) ** 0.50, 2)
-        crdc = 0.18/self.gc
-        vrdc = max(crdc*kv*(100*p1*self.fck)**(1/3),
+        p1 = min(astProv/(self.b*self.effectiveDepth), 0.02)
+        kv = min(1+(200/self.effectiveDepth)**0.50, 2)
+        cRdc = 0.18/self.gc
+        vRdc = max(cRdc*kv*(100*p1*self.fck)**(1/3),
                    0.035*kv**1.5*self.fck**0.50)
 
-        vcap = self.b * self.effective_depth * vrdc / 1000
-        print(f"The shear capcity of the section with no links = {vcap:.2f}kN")
+        VCap = self.b*self.effectiveDepth * vRdc/1000
+        print(f"The shear capcity of the section with no links = {VCap:.2f}kN")
 
-        return vcap
+        return VCap
 
-    def shear_cap_with_links(self, asw : float, sv : float, theta : float)->float:
+    def shearCapLinks(self, asw : float, sv : float, theta : float)->float:
         """ check the shear capacity of a section if shear reinforcement is provided """
         # check to make sure max link spacing provided
-        if(sv > 0.75*self.effective_depth):
+        if(sv > 0.75*self.effectiveDepth):
             print("The links spacing specified is greater than the max spacing")
         f = theta * math.pi/180
 
-        v = asw * self.effective_depth * 0.90 * 0.87 * self.fyk * (1 / math.tan(f)) / (sv * 1000)
+        v = asw*self.effectiveDepth*0.90*0.87*self.fyk*(1/math.tan(f))/(sv*1000)
 
         fcd = self.acc*self.fck/self.gc
 
-        self.vMax = self.b * self.effective_depth * 0.90 * 0.60 * (1 - self.fck / 250) * fcd / (1 / math.tan(f) + math.tan(f)) * (1 / 1000)
+        self.vMax = self.b*self.effectiveDepth*0.90*0.60 * (1-self.fck/250)*fcd/(1/math.tan(f)+math.tan(f))*(1/1000)
 
         vCap = min(v, self.vMax)
         print(f"The shear capacity of the section with links = {vCap:.2f} kN")
 
-        return vcap
+        return vCap
 
-    def span_to_depth(self, ast_req:float, ast_prov:float, comp_steel:float, K:float, span:float)->float:
+    def spanToDepth(self, astReq:float, astProv:float, compSteel:float, K:float, span:float)->float:
         """ method to calculate the allowable span to depth ratio """
         p0 = self.fck** 0.50 * 10 ** -3
-        p = ast_req / (self.b * self.effective_depth)
-        pc = comp_steel / (self.b * self.effective_depth)
+        p = astReq / (self.b * self.effectiveDepth)
+        pc = compSteel / (self.b * self.effectiveDepth)
 
         if p < p0:
             sdr = K * ((11 + 1.5 * self.fck**0.5 * (p0 / p) + 3.2 *
-                        (p0 / p - 1) ** 1.5) * (500 / (500 * ast_req / ast_prov)))
+                        (p0 / p - 1) ** 1.5) * (500 / (500 * astReq / astProv)))
         else:
             sdr = K * ((11 + 1.5 * self.fck ** 0.5 * (p0 / (p - pc)) + (1 / 12) * self.fck
-                        ** 0.50 * (pc / p0) ** 0.50) * (500 / (500 * ast_req / ast_prov)))
+                        ** 0.50 * (pc / p0) ** 0.50) * (500 / (500 * astReq / astProv)))
 
         print(f"Required maximum span to depth ratio = {sdr:.2f}")
-        ld = span * 10 ** 3 / self.effective_depth
+        ld = span * 10 ** 3 / self.effectiveDepth
         print(f"Actual spand to depth ratio = {ld:.2f}")
         return sdr
 
-    def max_torsion_cap(self, theta:float, asw:float, sv:float, ast_prov:float, Ved:float, Ted:float)->None:
-        vCap = self.shear_cap_with_links(asw, sv, theta)  # call to assign vMax
+    def maxTorsionCap(self, theta:float, asw:float, sv:float, astProv:float, Ved:float, Ted:float)->None:
+        vCap = self.shearCapLinks(asw, sv, theta)  # call to assign vMax
         t = (self.b * self.h) / (2 * (self.b + self.h))
         Ak = (self.h - t) * (self.b - t)
         uk = 2 * ((self.h - t) + (self.b - t))
-        tefi = max(t, 2 * (self.h - self.effective_depth))
+        tefi = max(t, 2 * (self.h - self.effectiveDepth))
         fcd = self.acc * self.fck / self.gc
         acw = 1
         v = 0.6 * (1 - (self.fck / 250))
@@ -108,19 +109,19 @@ class RectBeam(structuralConcrete.Concrete):
         fctd = self.fctk / self.gc
         TRdc = 2 * Ak * fctd * t * 10 ** -6
         print(f"Torsional cracking moment = {TRdc:.2f} kNm")
-        v_cap_no_links = self.shear_cap_no_links(ast_prov)
-        print(f"Unreinforced shear capacity of section = {v_cap_no_links:.2f} kN")
-        uRatioB = (Ted / TRdc) + (Ved / v_cap_no_links)
+        vCapNoLinks = self.shearCapNoLinks(astProv)
+        print(f"Unreinforced shear capacity of section = {vCapNoLinks:.2f} kN")
+        uRatioB = (Ted / TRdc) + (Ved / vCapNoLinks)
         print(f"Minimum reinforcement unity check = {uRatioB:.2f}")
         if uRatioB > 1:
             print(f"Design torsion reinforcement")
             ti = Ted * 10 ** 6 / (2 * Ak * t)
             print(f"Shear stress in effective wall = {ti:.2f} MPa")
-            Vt = ti * t * self.effective_depth * 10 ** -3
+            Vt = ti * t * self.effectiveDepth * 10 ** -3
             VTotal = Ved / 2 + Vt
             print(f"Shear force in a vertical wall = {Vt:.2f} kN")
             print(f"Total shear force in the vertical wall = {VTotal:.2f} kN")
-            vWallCap = self.shear_cap_with_links(asw / 2, sv, theta)
+            vWallCap = self.shearCapLinks(asw / 2, sv, theta)
             print(f"Shear capacity of the wall = {vWallCap:.2f} kN")
             if vWallCap > VTotal:
                 print("Torsional capacity okay")
